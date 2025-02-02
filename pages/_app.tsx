bc15579d0ac79f8b2d3dae3c44dbf3304d9c58ae
WayneStaticScientist <vanderlinkinchat@gmail.com>
2025-02-02 10:12:13 +0200

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,9 @@
 import "@/styles/globals.css";
+import "../public/assets/css/style.css";
+import "../public/assets/css/bootstrap.min.css";
+import "../public/assets/css/fontawesome.min.css";
+import "../public/assets/css/swiper-bundle.min.css";
+import "../public/assets/css/magnific-popup.min.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,6 +9,15 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      <Script src="../public/assets/js/vendor/jquery-3.7.1.min.js"></Script>
+      <Script src="../public/assets/js/swiper-bundle.min.js"></Script>
+      <Script src="../public/assets/js/bootstrap.min.js"></Script>
+      <Script src="../public/assets/js/jquery.magnific-popup.min.js"></Script>
+      <Script src="../public/assets/js/jquery.counterup.min.js"></Script>
+      <Script src="../public/assets/js/jquery-ui.min.js"></Script>
+      <Script src="../public/assets/js/imagesloaded.pkgd.min.js"></Script>
+      <Script src="../public/assets/js/isotope.pkgd.min.js"></Script>
+      <Script src="../public/assets/js/main.js"></Script>
     </Html>
   );
 }
