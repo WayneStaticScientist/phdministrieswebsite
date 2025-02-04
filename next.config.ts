8673810467e1c86c4ba56bcfa0fe1de3015a377c
WayneStaticScientist <vanderlinkinchat@gmail.com>
2025-02-04 19:28:55 +0200

    dv

--- next.config.ts
+++ next.config.ts
@@ -4,5 +4,38 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 };
+module.exports = {
+  // ...existing code...
+  webpack(config: {
+    module: {
+      rules: {
+        test: RegExp; use: (string | {
+          loader: string; options: {
+            // ... other css-loader options ...
+            importLoaders: number;
+          };
+        })[];
+      }[];
+    };
+  }) {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        process.env.NODE_ENV === 'development' ? 'style-loader' : require.resolve('null-loader'), // Correct check
+        {
+          loader: 'css-loader',
+          options: {
+            // ... other css-loader options ...
+            importLoaders: 1, // If you are using PostCSS
+          },
+        },
+        'postcss-loader', // if you are using PostCSS
+      ],
+    });
+
+    return config;
+  },
+  // ...existing code...
+};
 
 export default nextConfig;
---
+++ postcss.config.ts
@@ -0,0 +1,10 @@
+import autoprefixer from 'autoprefixer';
+
+const config = {
+    plugins: [
+        autoprefixer, // Add autoprefixer
+        // ... other PostCSS plugins
+    ],
+};
+
+export default config;
